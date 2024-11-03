e547e4f61d661bff21a1ac1f88b37d6be7b87c6c
Patateon <maximebeldjilali@gmail.com>
2024-11-03 17:58:27 +0100

    initial cnn classifier commit

---
+++ code/cnn/classifier_training.py
@@ -0,0 +1,70 @@
+
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.keras import datasets, models, layers
+
+import numpy as np
+import tensorflow
+
+
+# Load dataset
+# Cifar-10 (cf photomosaic)
+(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+
+# Normalisation des images 
+x_max = x_train.max()
+x_train = x_train / x_max
+x_test = x_test / x_max
+
+"""
+# Création du modèle (à tune plus tard)
+
+Shape :
+ - En entrée : (?, 28, 28, 3) 
+ - En sortie : (10)
+
+Actuellement :
+ - 3 couches de convolution avec 32, 64 et 128 filtres
+ - Un MaxPooling2D de 2,2
+ - Un dropout à 2 pour l'overfitting (à voir)
+ - relu comme fonction d'activation et un softmax à la fin
+
+"""
+
+model = models.Sequential()
+
+model.add(layers.Input((32, 32, 3)))
+
+model.add(layers.Conv2D(32, (3, 3), activation = 'relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3), activation = 'relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+# model.add(layers.Conv2D(128, (3, 3), activation = 'relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(100, activation = 'relu'))
+model.add(layers.Dropout(0.2))
+model.add(layers.Dense(10, activation = 'softmax'))
+
+model.summary()
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy',
+              metrics = ['accuracy'])
+
+batch_size = 512
+epochs = 8
+
+history = model.fit(x_train, y_train,
+          batch_size = batch_size,
+          epochs = epochs,
+          verbose = True,
+          validation_data = (x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose = 0)
+
+print(f"Test loss : {score[0]:4.4f}")
+print(f"Test accuracy : {score[1]:4.4f}")

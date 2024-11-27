ad9c0808bd955e0f01f55ccec6cc63642a39f381
Patateon <maximebeldjilali@gmail.com>
2024-11-27 11:57:36 +0100

    update obscurator.py

--- code/cnn/obscurator.py
+++ code/cnn/obscurator.py
@@ -20,8 +20,22 @@ class Application(tk.Tk):
         self.geometry("640x480")
         self.resizable(0, 0)
         
+        # Create a menu bar
+        self.create_menu_bar()
+        
         # Initialize the main frame 
         self.scene_initializer()
+
+    def create_menu_bar(self):
+        menu_bar = tk.Menu(self)
+    
+        menu_file = tk.Menu(menu_bar, tearoff=0)
+        menu_file.add_command(label="Open...", command=self.open_file)
+        menu_file.add_separator()
+        menu_file.add_command(label="Exit", command=self.quit)
+        menu_bar.add_cascade(label="File", menu=menu_file)
+        
+        self.config(menu=menu_bar)
 
 
     def scene_initializer(self):
@@ -31,14 +45,20 @@ class Application(tk.Tk):
 
         main_frame = tk.Frame(self)
         
-        menu_bar = tk.Menu(main_frame)
-        menu_bar.config(menu=menu_bar)
+        self.image_frame = tk.Frame(self)
+        self.canvas = tk.Canvas(self.image_frame, bg="gray", width=512, height=512)
         
-        file_menu = tk.Menu(main_frame)
-        file_menu.add_command(label="Quit", command=self.quit)
-        menu_bar.add_cascade(label="File", menu=menu_bar)
-
+        self.image_frame.pack(expand=True)
+        self.canvas.pack(expand=True)
+        
         main_frame.pack()
+        
+
+    def display_image(self, image_path):
+        self.current_image = ImageTk.PhotoImage(Image.open(image_path))
+        
+        self.canvas.delete("all")
+        self.canvas.create_image(20, 20, anchor=tk.NW, image=self.current_image)
 
 
     def open_file(self):
@@ -53,9 +73,11 @@ class Application(tk.Tk):
 
         if not path:
             return
-
-        self.text_in.delete(0, 'end')
-        self.text_in.insert(0, path)
+        
+        if self.image_frame is not None:
+            self.display_image(path)
+            
+        print(path)
 
 
     def open_dir(self):
@@ -65,9 +87,6 @@ class Application(tk.Tk):
 
         if not path:
             return
-
-        self.text_data.delete(0, 'end')
-        self.text_data.insert(0, path)
 
 
     def save_file(self):
@@ -83,9 +102,6 @@ class Application(tk.Tk):
 
         if not path:
             return
-
-        self.text_out.delete(0, 'end')
-        self.text_out.insert(0, path)
      
 
 if __name__ == "__main__":
